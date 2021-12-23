7856bb18c7e65c0e489569c83dd23fa55d46de12
Hammad <hmdshfq@outlook.com>
2021-12-23 19:14:25 +0100

    add accordion content

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,10 +1,22 @@
 import React from 'react'
 
-
-export default ({items}) => {
+export default ({ items }) => {
+    const renderedItems = items.map(item => {
+        return (
+            <React.Fragment key={item.title}>
+                <div className='title active'>
+                    <i className='dropdown icon'></i>
+                    {item.title}
+                </div>
+                <div className='content active'>
+                    <p>{item.content}</p>
+                </div>
+            </React.Fragment>
+        )
+    })
     return (
-        <div>
-            {items.length}
+        <div className='ui styled accordion'>
+            {renderedItems}
         </div>
     )
 }
