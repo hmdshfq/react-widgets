7f83dd5cb05818328f69d900e7f4c85ee811e1e4
Hammad <hmdshfq@outlook.com>
2022-07-27 21:30:20 +0200

    complete accordion

--- src/components/Accordion.jsx
+++ src/components/Accordion.jsx
@@ -1,22 +1,29 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default ({ items }) => {
-    const renderedItems = items.map(item => {
-        return (
-            <React.Fragment key={item.title}>
-                <div className='title active'>
-                    <i className='dropdown icon'></i>
-                    {item.title}
-                </div>
-                <div className='content active'>
-                    <p>{item.content}</p>
-                </div>
-            </React.Fragment>
-        )
-    })
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+  const onTitleClick = index => {
+    setActiveIndex(index);
+  };
+  const renderedItems = items.map((item, index) => {
+    const activeClass = index === activeIndex ? 'active' : '' 
     return (
-        <div className='ui styled accordion'>
-            {renderedItems}
+      <React.Fragment key={item.title}>
+        <div className={`title ${activeClass}`} onClick={() => onTitleClick(index)}>
+          <i className='dropdown icon'></i>
+          {item.title}
         </div>
-    )
-}
+        <div className={`content ${activeClass}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+  return (
+    <div className='ui styled accordion'>
+      {renderedItems}
+    </div>
+  );
+};
+
+export default Accordion;
